3b194037d6f19f64761806698d629895ea4c6e2a
keprom <kokshetau.energo.prom@mail.ru>
2018-11-28 16:37:41 +0600

    added checking before deleting tu

--- system/application/controllers/billing.php
+++ system/application/controllers/billing.php
@@ -553,12 +553,15 @@ class Billing extends Controller
 		$this->db->insert("industry.billing_point",$_POST);
 		redirect("billing/firm/".$_POST['firm_id']);
 	}
+
 	function close_billing_point()
 	{
+        $this->check_billing_point($this->uri->segment(3));
 		$sql="update industry.billing_point set deleted_point=true where id=".$this->uri->segment(3);
 		$this->db->query($sql);
 		redirect("billing/firm/".$this->uri->segment(4));
 	}
+
 	function tp_billing_point()
 	{
 		$sql="update industry.billing_point set in_tp= not in_tp where id=".$this->uri->segment(3);
@@ -1170,6 +1173,34 @@ class Billing extends Controller
         }
 		$this->load->view("reports/schetoplata",$data);
 	}
+
+    private function check_billing_point($bill_id)
+    {
+        $bill_id = $this->uri->segment(3);
+        $period_id = $this->get_cpi($bill_id);
+        $this->db->where("bill_id",$bill_id);
+        $this->db->where("period_id",$period_id);
+        $n = $this->db->get("industry.nadbavka_info");
+        if($n->num_rows > 0){
+            die("V dannyi period na tochke ucheta nahodyatsya nadbavki!");
+        }
+        $this->db->where("bill_id",$bill_id);
+        $this->db->where("period_id",$period_id);
+        $sbp = $this->db->get("industry.sovm_billing_point");
+        if($sbp->num_rows > 0){
+            die("Na tochke ucheta imeutsya sovmesntye uchety!");
+        }
+        $this->db->where("bill_id",$bill_id);
+        $unfc = $this->db->get("industry.unfinished_counter");
+        if($unfc->num_rows > 0){
+            die("Na tochke ucheta imeutsya nesnyatye schetchiki!");
+        }
+    }
+
+    private function get_cpi(){
+        return $this->db->query("select * from industry.current_period_id()")->row()->current_period_id;
+    }
+
 	function akt()
 	{
 		$query="SELECT * FROM industry.current_akt WHERE values_set_id=".$this->uri->segment(3);
@@ -1501,6 +1532,7 @@ class Billing extends Controller
 	
 	function delete_billing_point()
 	{
+        $this->check_billing_point($this->uri->segment(3));
 		$sql="select firm_id from industry.billing_point where id=".$this->uri->segment(3);
 		$firm_id=$this->db->query($sql)->row()->firm_id;
 		$sql="select count(*) as count from industry.counter where point_id=".$this->uri->segment(3);
