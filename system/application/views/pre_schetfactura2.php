2031e8d26dbee0e034cdd0bf17e00d16415372a0
keprom <kokshetau.energo.prom@mail.ru>
2019-04-17 16:43:27 +0600

    fix gruzootpravitel

--- system/application/views/pre_schetfactura2.php
+++ system/application/views/pre_schetfactura2.php
@@ -1,28 +1,70 @@
-<?php
-$r=$r->row();
-echo form_open("billing/schetfactura");
-echo "<input type=hidden name=firm_id value=".$firm_id." >";
-echo "<input type=checkbox name=html  ><br>";
-echo "Выдать актом выполненых работ <input type=checkbox name=akt_vypolnenyh_rabot  ><br>";
-echo "Новая счет-фактура <input type=checkbox name=new_schetfactura ><br>";
-echo "Выдача накладной <input type=checkbox name=nakladnaya><br>";
-echo "<input type=hidden name=period_id value=".$period_id." >";
-?>
-Другая дата выдачи: <input name=data_schet value='<?php echo $r->data_schet; ?>' > <br><br>
-Другой номер счетфактуры: <input name=schet2 value='<?php echo $r->schet2; ?>' > <br><br>
-Последний номер счета-фактуры <input type="text" disabled value="<?php echo $max_schet_number; ?>"><br><br>
-Условия оплаты по договору: <input name=edit1 value='<?php echo $firm->edit1; ?>' > <br><br>
-Пункт назначения поставляемых товаров (работ, услуг): <input name=edit2 value='<?php echo $firm->edit2; ?>'> <br> <br>
-Поставка товаров осуществлена по доверености: <input name=edit3 value='<?php echo $firm->edit3; ?>'> <br> <br>
-Способ отправления: <input name=edit4  value='<?php echo $firm->edit4; ?>'> <br> <br>
-Грузоотправитель:<input name=edit5  value='<?php echo $firm->edit5; ?>'> <br> <br>
-Грузополучатель:<input name=edit6  value='<?php echo $firm->edit6; ?>'> <br> <br>
-Другой номер договора:<input name=dog2  value='<?php echo $firm->dog2; ?>'> <br> <br>
-
-<?php
-
-echo "<input type=submit value='Открыть счетфактуру' />";
-echo "</form>";
-
-
-?>
+<?php echo form_open("billing/schetfactura"); ?>
+    <input type="hidden" name="period_id" value="<?php echo $period_id; ?>">
+    <input type="hidden" name="firm_id" value="<?php echo $firm_id; ?>">
+    <table>
+        <tr>
+            <td>Выдача накладной</td>
+            <td><input type=checkbox name=nakladnaya></td>
+        </tr>
+        <tr>
+            <td>Другая дата выдачи:</td>
+            <td><input name=data_schet value='<?php echo $r->data_schet; ?>'></td>
+        </tr>
+        <tr>
+            <td>Другой номер счетфактуры:</td>
+            <td><input name=schet2 value='<?php echo $r->schet2; ?>'></td>
+        </tr>
+        <tr>
+            <td>Последний номер счета-фактуры</td>
+            <td><input type="text" disabled value="<?php echo $max_schet_number; ?>"></td>
+        </tr>
+        <tr>
+            <td>Условия оплаты по договору:</td>
+            <td><input name=edit1 value='<?php echo $firm->edit1; ?>'></td>
+        </tr>
+        <tr>
+            <td>Пункт назначения поставляемых товаров (работ, услуг):</td>
+            <td><input name=edit2 value='<?php echo $firm->edit2; ?>'></td>
+        </tr>
+        <tr>
+            <td>Поставка товаров осуществлена по доверености:</td>
+            <td><input name=edit3 value='<?php echo $firm->edit3; ?>'></td>
+        </tr>
+        <tr>
+            <td>Способ отправления:</td>
+            <td><input name=edit4 value='<?php echo $firm->edit4; ?>'></td>
+        </tr>
+        <tr>
+            <td>Грузоотправитель:</td>
+            <td><input name="edit5" value='<?php echo $firm->edit5; ?>'></td>
+        </tr>
+        <tr>
+            <td>Грузополучатель:</td>
+            <td><textarea name="gruzopoluchatel"
+                          id="gruzopoluchatel"
+                          cols="50"
+                          rows="3"><?php echo trim($firm->bin) . ", " . $firm->name . " " . $firm->address; ?></textarea>
+            </td>
+        </tr>
+        <tr>
+            <td>Получатель:</td>
+            <td><input type="text"
+                       name="pokupatel"
+                       value='<?php echo($firm->name); ?>'></td>
+        </tr>
+        <tr>
+            <td>БИН и адрес нахождения покупателя:</td>
+            <td><textarea name="pokupatel_address"
+                          id="pokupatel_address"
+                          cols="50"
+                          rows="3"><?php echo trim($firm->bin) . ", " . $firm->address; ?></textarea></td>
+        </tr>
+        <tr>
+            <td>Другой номер договора:</td>
+            <td><input name=dog2 value='<?php echo $firm->dog2; ?>'></td>
+        </tr>
+        <tr>
+            <td colspan="2"><input type=submit value='Открыть счетфактуру'/></td>
+        </tr>
+    </table>
+<?php form_close(); ?>
